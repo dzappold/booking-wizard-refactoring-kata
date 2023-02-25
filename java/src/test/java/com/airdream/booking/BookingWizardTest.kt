00fb7765460900b5f746261d6638091504f10f7b
Daniel <daniel.zappold@gmx.de>
2023-02-25 21:59:50 +0100

    :recycle: improve readability of tests

--- java/src/test/java/com/airdream/booking/BookingWizardTest.kt
+++ java/src/test/java/com/airdream/booking/BookingWizardTest.kt
@@ -8,246 +8,181 @@ import org.junit.jupiter.api.Test
 class BookingWizardTest {
 
     @Test
-    fun `outward flight with 2 passengers`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "1",
-                "Frankfurt",
-                "London",
-                "10/12/2024",
-                "2",
-                "Michael Collins",
-                "Sue Morrison",
-                "Yes"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `outward flight with 2 passengers`() =
+        validateFor(
+            "1",
+            "Frankfurt",
+            "London",
+            "10/12/2024",
+            "2",
+            "Michael Collins",
+            "Sue Morrison",
+            "Yes"
+        )
 
     @Test
-    fun `round trip flight with 4 passengers`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/09/2024",
-                "4",
-                "James Hetfield",
-                "Marie",
-                "Michael Collins",
-                "Sue Morrison",
-                "Yes"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `round trip flight with 4 passengers`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/09/2024",
+            "4",
+            "James Hetfield",
+            "Marie",
+            "Michael Collins",
+            "Sue Morrison",
+            "Yes"
+        )
 
     @Test
-    fun `round trip flight with 4 passengers no confirmation`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/09/2024",
-                "4",
-                "James Hetfield",
-                "Marie",
-                "Michael Collins",
-                "Sue Morrison",
-                "No"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `round trip flight with 4 passengers no confirmation`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/09/2024",
+            "4",
+            "James Hetfield",
+            "Marie",
+            "Michael Collins",
+            "Sue Morrison",
+            "No"
+        )
 
     @Test
-    fun `invalid trip`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "7",
-                "abc",
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/09/2024",
-                "4",
-                "James Hetfield",
-                "Marie",
-                "Michael Collins",
-                "Sue Morrison",
-                "No"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `invalid trip`() =
+        validateFor(
+            "7",
+            "abc",
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/09/2024",
+            "4",
+            "James Hetfield",
+            "Marie",
+            "Michael Collins",
+            "Sue Morrison",
+            "No"
+        )
 
     @Test
-    fun `departure in the past`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "7",
-                "abc",
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/1999",
-                "26/07/2024",
-                "26/09/2024",
-                "4",
-                "James Hetfield",
-                "Marie",
-                "Michael Collins",
-                "Sue Morrison",
-                "No"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `departure in the past`() =
+        validateFor(
+            "7",
+            "abc",
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/1999",
+            "26/07/2024",
+            "26/09/2024",
+            "4",
+            "James Hetfield",
+            "Marie",
+            "Michael Collins",
+            "Sue Morrison",
+            "No"
+        )
 
     @Test
-    fun `invalid departure date`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "7",
-                "abc",
-                "2",
-                "Frankfurt",
-                "London",
-                "26#07--1999",
-                "26/07/2024",
-                "26/09/2024",
-                "4",
-                "James Hetfield",
-                "Marie",
-                "Michael Collins",
-                "Sue Morrison",
-                "No"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
-    @Test
-    fun `return date in the past`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/07/1999",
-                "26/09/2024",
-                "1",
-                "Sue Morrison",
-                "Yes"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `invalid departure date`() =
+        validateFor(
+            "7",
+            "abc",
+            "2",
+            "Frankfurt",
+            "London",
+            "26#07--1999",
+            "26/07/2024",
+            "26/09/2024",
+            "4",
+            "James Hetfield",
+            "Marie",
+            "Michael Collins",
+            "Sue Morrison",
+            "No"
+        )
 
     @Test
-    fun `return date before departure`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/07/2023",
-                "26/09/2024",
-                "1",
-                "Sue Morrison",
-                "Yes"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `return date in the past`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/07/1999",
+            "26/09/2024",
+            "1",
+            "Sue Morrison",
+            "Yes"
+        )
+
     @Test
-    fun `return date invalid format`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26#07--2023",
-                "26/09/2024",
-                "1",
-                "Sue Morrison",
-                "Yes"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `return date before departure`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/07/2023",
+            "26/09/2024",
+            "1",
+            "Sue Morrison",
+            "Yes"
+        )
+
     @Test
-    fun `invalid passenger number`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/09/2024",
-                "abc",
-                "1",
-                "Sue Morrison",
-                "Yes"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `return date invalid format`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26#07--2023",
+            "26/09/2024",
+            "1",
+            "Sue Morrison",
+            "Yes"
+        )
+
     @Test
-    fun `invalid confirmation`() {
-        val output = tapSystemErrAndOutNormalized {
-            withTextFromSystemIn(
-                "2",
-                "Frankfurt",
-                "London",
-                "26/07/2024",
-                "26/09/2024",
-                "1",
-                "Sue Morrison",
-                "something",
-                "No"
-            ).execute {
-                val wizard = BookingWizard()
-                wizard.start()
-            }
-        }
-        verify(output)
-    }
+    fun `invalid passenger number`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/09/2024",
+            "abc",
+            "1",
+            "Sue Morrison",
+            "Yes"
+        )
+
+    @Test
+    fun `invalid confirmation`() =
+        validateFor(
+            "2",
+            "Frankfurt",
+            "London",
+            "26/07/2024",
+            "26/09/2024",
+            "1",
+            "Sue Morrison",
+            "something",
+            "No"
+        )
 }
+
+private fun validateFor(vararg input: String) =
+    verify(
+        tapSystemErrAndOutNormalized {
+            withTextFromSystemIn(*input)
+                .execute { BookingWizard().start() }
+        }
+    )
